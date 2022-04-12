4064454432c888e8bcda67e4c7d1b9424106fb55
liorshif <lior>
2022-04-12 16:35:19 +0300

    added necessary dependencies

--- src/test/java/Posts.java
+++ src/test/java/Posts.java
@@ -20,45 +20,45 @@ public class Posts {
         return Objects.hashCode(userId, id, title, body);
     }
 
-    public Posts() {
+    private Posts() {
     }
 
-    public Posts(int userId, int id, String title, String body) {
+    private Posts(int userId, int id, String title, String body) {
         this.userId = userId;
         this.id = id;
         this.title = title;
         this.body = body;
     }
 
-    public int getUserId() {
+    private int getUserId() {
         return userId;
     }
 
-    public void setUserId(int userId) {
+    private void setUserId(int userId) {
         this.userId = userId;
     }
 
-    public int getId() {
+    private int getId() {
         return id;
     }
 
-    public void setId(int id) {
+    private void setId(int id) {
         this.id = id;
     }
 
-    public String getTitle() {
+    private String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    private void setTitle(String title) {
         this.title = title;
     }
 
-    public String getBody() {
+    private String getBody() {
         return body;
     }
 
-    public void setBody(String body) {
+    private void setBody(String body) {
         this.body = body;
     }
 }
